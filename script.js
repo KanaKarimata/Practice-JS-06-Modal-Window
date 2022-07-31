8c502bb0186e18b3417c80be2123d9a1dd078e26
KanaKarimata <kana.lt22@gmail.com>
2022-07-31 11:22:38 +0900

    Finish Part7-79

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 'use strict';
+
+const modal = document.querySelector(`.modal`);
+const overlay = document.querySelector(`.overlay`);
+const btnCloseModal = document.querySelector(`.close-modal`);
+//querySelector matches only the first element even if there are multiple same class
+const btnsOpenModal = document.querySelectorAll(`.show-modal`);
+console.log(btnsOpenModal);
+
+for(let i = 0; i < btnsOpenModal.length; i++)
+  console.log(btnsOpenModal[i].textContent);
